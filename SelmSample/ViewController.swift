e2fa0c5ff87f3fe822367260ccf6eed6fb97e2a0
Ryoichi Izumita <r.izumita@caph.jp>
2019-04-08 07:50:07 +0900

    Initial Commit

---
+++ SelmSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SelmSample
+//
+//  Created by 和泉田 領一 on 2019/04/08.
+//  Copyright © 2019 CAPH TECH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
